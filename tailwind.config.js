9fee534cbb3297b92a05354d7e66024e1e7d9229
ahirraorohan <150143780+ahirraorohan@users.noreply.github.com>
2024-11-08 16:18:26 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        pastelPink: '#FFB6C1',
+        pastelBlue: '#B3CDE0',
+        pastelYellow: '#FFFACD',
+        pastelPurple: '#E6E6FA',
+      },
+    },
+  },
+};
